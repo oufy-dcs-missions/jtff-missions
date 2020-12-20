2509311e8c3589ba5ebc0ee654a1e542e342255d
Sebastien LONGO <sebastien.longo@oufy.net>
2020-12-21 00:12:38 +0100

    feat(final_countdown): create mission from the movie

---
+++ final_countdown/resources/Liveries/e-2c/VAW-124_161782 - Used/description.lua
@@ -0,0 +1,17 @@
+livery = {
+	{"e2d_fus", 0 ,"e2d_fus_161782",false};
+	{"e2d_fus", ROUGHNESS_METALLIC ,"e2d_fus_RoughMet",false};
+	{"e2d_wing", 0 ,"e2d_wing_161782",false};
+	{"e2d_wing", ROUGHNESS_METALLIC ,"e2d_wing_RoughMet",false};
+	
+	{"e2d_pilot", 0, "pilot_e2_vaw124", false};
+	{"e2d_wso", 0, "wso_e2_vaw124", false};
+	{"e2d_pilot_helmet", 0, "hgu55_vaw124.dds", false};
+
+	{"f15numbers", 0, "empty", true};
+	
+}
+	
+name = "VAW-124 - 161782 (worn)"
+
+countries = {"USA"}
---
+++ final_countdown/resources/Liveries/s-3b tanker/USAF - VS-24 - CAG/description.lua
@@ -0,0 +1,27 @@
+-- rename it to description.lua
+livery = 
+{
+    {"texture S3-OKRAS.bmp f2 g0", 0, "S3-OKRAS-VS24.dds", false};
+    {"texture F-14-NAVY.tga f2 g0", 0, "F-14-NAVY.dds", false};
+    {"texture nomera.tga f2 g0", 0, "nomera.dds", false};
+    {"texture nomera-black.tga f2 g0", 0, "nomera-BLACK.dds", false};
+    {"texture nomera-red.tga f2 g0", 0, "nomera-red.dds", false};
+    {"texture SOPLA-F.bmp f2 g0", 0, "sopla-f.dds", false};
+    {"texture PILOT-USA-AF-OKRAS.BMP f2 g0", 0, "Pilot_vs24.dds", false};
+    {"texture PILOT-USA-HOLE.bmp f2 g0", 0, "pilot-usa-hole.dds", false};
+	--{"texture BANO.tga f0 g0", 0, "BANO.tga", false};
+    --{"texture BANO.tga f0 g0", 8, "BANO.tga", false};
+    --{"texture DAMAGE_Su25.tga f2 g0", 0, "DAMAGE_Su25.tga", };
+    --{"texture EJECTSEAT.bmp f2 g0", 0, "EJECTSEAT.bmp", true};
+    --{"texture FONAR-25.tga f2 g0", 0, "FONAR-25.tga", true};
+    --{"texture HOLES_All.tga f2 g0", 0, "HOLES_All.tga", true};
+    --{"texture LONGER-DAMAG.bmp f2 g0", 0, "LONGER-DAMAG.bmp", true};
+    --{"texture PILOT-USA-HOLE.bmp f2 g0", 0, "PILOT-USA-HOLE.bmp", true};
+    --{"texture nomera-red.tga f2 g0", 0, "nomera-red.tga", true};
+}
+name = "US NAVY - VS-24 CAG"
+--name_xx = ""
+--[[ assign the countries
+     if you want no country limitation,
+     then comment out below line]]
+countries = {"USA"}
