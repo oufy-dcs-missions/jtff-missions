e4af5c3be4eeb97d8d595a6a85a6f595232484d7
Sebastien LONGO <sebastien.longo@oufy.net>
2020-12-21 00:11:34 +0100

    feat(vf84-qualif): add planes on red side

--- vf84-qualification/src/vf84-qualification-init.lua
+++ vf84-qualification/src/vf84-qualification-init.lua
@@ -13,9 +13,9 @@ Scheduler = SCHEDULER:New( nil )
 -- Spawns Tankers
 --
 spawnTexaco = SPAWN:New("texaco")
-spawnShell = SPAWN:New("shell")
+--spawnShell = SPAWN:New("shell")
 spawnEscortTexaco = SPAWN:New("escort-texaco")
-spawnEscortShell = SPAWN:New("escort-shell")
+--spawnEscortShell = SPAWN:New("escort-shell")
 --
 -- Civilian aircrafts
 --
@@ -238,11 +238,11 @@ groupTexaco:GetUnit(1):GetBeacon():AATACAN(15, "TXC", true)
 --
 -- Shell --
 --
-groupShell = spawnShell:InitSkill("Excellent"):Spawn()
-groupEscortShell = spawnEscortShell:InitSkill("Excellent"):Spawn():OptionAlarmStateRed():OptionROEReturnFire()
-taskEscortShell = groupEscortShell:TaskFollow(groupShell,ObjEspacementTankers:GetVec3(), 4)
-groupEscortShell:SetTask( taskEscortShell, 1 )
-groupShell:GetUnit(1):GetBeacon():AATACAN(14, "SHL", true)
+--groupShell = spawnShell:InitSkill("Excellent"):Spawn()
+--groupEscortShell = spawnEscortShell:InitSkill("Excellent"):Spawn():OptionAlarmStateRed():OptionROEReturnFire()
+--taskEscortShell = groupEscortShell:TaskFollow(groupShell,ObjEspacementTankers:GetVec3(), 4)
+--groupEscortShell:SetTask( taskEscortShell, 1 )
+--groupShell:GetUnit(1):GetBeacon():AATACAN(14, "SHL", true)
 -- *****************************************************************************
 --                     **                 bomber Raid                         **
 --                     *********************************************************
@@ -264,7 +264,7 @@ local MenuCoalitionBlue = MENU_COALITION:New( coalition.side.BLUE, "Game Master 
 
 -- Menus tankers
 menuResetTexaco = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "reset Texaco", MenuCoalitionBlue , fctResetTankerAndEscort, {spawnTexaco, spawnEscortTexaco, "texaco", 15, "TXC"})
-menuResetShell = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "reset Shell", MenuCoalitionBlue , fctResetTankerAndEscort, {spawnShell, spawnEscortShell, "shell", 14, "SHL"})
+--menuResetShell = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "reset Shell", MenuCoalitionBlue , fctResetTankerAndEscort, {spawnShell, spawnEscortShell, "shell", 14, "SHL"})
 menuResetOverlord = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "reset Overlord", MenuCoalitionBlue , fctResetAWACS, {spawnoverlord, spawnEscortOverlord, "overlord"})
 -- Menus BVR
 local MenuBVRBlue = MENU_COALITION:New( coalition.side.BLUE, "BVR Blue" )
@@ -284,3 +284,292 @@ menuSA3Off = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "SA3 Inactif", Men
 menuAGOn = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Groupe blindé défendu", MenuAGBlue, fctActivateSAM, {"groupe_blinde", false, 45} )
 menuAGOff = MENU_COALITION_COMMAND:New( coalition.side.BLUE, "Groupe blindé non-défendu", MenuAGBlue, fctSuspendSAM, {"groupe_blinde", false, 45} )
 
+-- *****************************************************************************
+-- *                         AIRBOSS settings                                  *
+-- *****************************************************************************
+airboss_enable = true
+
+airboss_enable_markzones = true
+
+airboss_enable_smokezones = false
+
+airboss_enable_niceguy = true
+
+airboss_enable_tanker = true
+airboss_enable_rescue_helo = true
+airboss_enable_awacs = true
+
+airbossCVN = nil
+carrierTanker = nil  --Ops.RecoveryTanker#RECOVERYTANKER
+RescueHelo = nil
+CVNAwacs = nil
+
+if airboss_enable == true then
+
+  airbossCVN = AIRBOSS:New("roosevelt", "roosevelt")
+  airbossCVN:SetTACAN(71, "X", "RSV")
+  airbossCVN:SetICLS(11, "RSVLSO")
+  airbossCVN:SetLSORadio(126.5)
+  airbossCVN:SetMarshalRadio(251.75)
+  airbossCVN:SetPatrolAdInfinitum(true)
+  airbossCVN:SetCarrierControlledArea(50)
+  airbossCVN:SetStaticWeather(true)
+  airbossCVN:SetRecoveryCase(3)
+  airbossCVN:SetMaxLandingPattern(5)
+  airbossCVN:SetDefaultPlayerSkill(AIRBOSS.Difficulty.NORMAL) -- other options EASY / HARD
+  airbossCVN:SetHandleAION()
+  airbossCVN:SetMenuRecovery(30, 20, true)
+  airbossCVN:SetMenuMarkZones(airboss_enable_markzones)
+  airbossCVN:SetMenuSmokeZones(airboss_enable_smokezones)
+  airbossCVN:SetAirbossNiceGuy(airboss_enable_niceguy)
+  airbossCVN:SetRadioRelayMarshal("MarshallRelay")
+  airbossCVN:SetRadioRelayLSO("LSORelay")
+  airbossCVN:SetSoundfilesFolder("Airboss Soundfiles/")
+  airbossCVN:SetMenuSingleCarrier()
+  --airbossCVN:Load(nil, "Greenie Board.csv")
+  --airbossCVN:SetAutoSave(nil, "Greenie Board.csv")
+
+  -- create fake recovery window at the end of the mission play
+  local window1 = airbossCVN:AddRecoveryWindow("15:00", "16:30", 3, 30, true, 20, false)
+  local window2 = airbossCVN:AddRecoveryWindow("18:00", "20:30", 3, 30, true, 20, false)
+
+  airbossCVN:Start()
+
+  if airboss_enable_tanker == true then
+    carrierTanker = RECOVERYTANKER:New(UNIT:FindByName("roosevelt"), "shell-1")
+    carrierTanker:SetUseUncontrolledAircraft()
+    function carrierTanker:OnAfterStart(From,Event,To)
+      airbossCVN:SetRecoveryTanker(carrierTanker)
+    end
+    carrierTanker:SetTACAN(14, "SHL")
+    carrierTanker:SetRadio(276.2, "AM")
+    carrierTanker:SetRespawnOn()
+    carrierTanker:SetAltitude(12500)
+    carrierTanker:SetSpeed(270)
+    carrierTanker:SetRacetrackDistances(15,25)
+    carrierTanker:SetRecoveryAirboss(true)
+    carrierTanker:SetModex(702)
+    carrierTanker:Start()
+  end
+
+  if airboss_enable_awacs == true then
+    CVNAwacs=RECOVERYTANKER:New(UNIT:FindByName("roosevelt"), "wizzard-1")
+    CVNAwacs:SetUseUncontrolledAircraft()
+    CVNAwacs:SetAWACS()
+    CVNAwacs:SetRadio(261, "AM")
+    CVNAwacs:SetAltitude(28000)
+    CVNAwacs:SetSpeed(270)
+    CVNAwacs:SetRespawnOn()
+    CVNAwacs:SetCallsign(CALLSIGN.AWACS.Wizard)
+    CVNAwacs:SetRacetrackDistances(30, 30)
+    CVNAwacs:SetModex(611)
+    CVNAwacs:SetTACAN(16, "WZD")
+    CVNAwacs:SetRecoveryAirboss(true)
+    CVNAwacs:Start()
+
+  end
+
+  if airboss_enable_rescue_helo == true then
+    RescueHelo =RESCUEHELO:New(UNIT:FindByName("roosevelt"), "Pedro-1")
+    RescueHelo:SetUseUncontrolledAircraft()
+    RescueHelo:SetTakeoffCold()
+    RescueHelo:Start()
+  end
+
+  env.info('Airboss ... OK')
+  trigger.action.outText("AIRBOSS scripts Loaded...", 10)
+  -- ADD TO CONFIG FILE
+  -- airboss_enable_markzones = true
+  -- airboss_enable_smokezones = true
+  -- airboss_enable_niceguy = true
+
+  timer.scheduleFunction(function()
+    trigger.action.outText(	"<< If the AIRBOSS option does not appear in your F10 - Other Menu, try switching slots a few times and you will get the AIRBOSS message popups! Check the AIRBOSS documentation (link in briefing for more info) >>", 30)
+  end, nil, timer.getTime() + 30  )
+
+end
+
+if airboss_enable == false then
+
+  timer.scheduleFunction(function()
+    trigger.action.outText("AIRBOSS script disabled...", 10)
+  end, nil, timer.getTime() + 8  )
+
+end
+
+
+-- *****************************************************************************
+-- *                         RAT settings                                      *
+-- *****************************************************************************
+--ADJUST RANDOM AI AIR TRAFFIC SPAWN SETTINGS HERE
+--READ THE MISSION SETTINGS --README FILE FOR MORE DETAILS
+
+random_civ_air_traffic = true
+
+
+-- # of civ aircraft spawns PER TYPE,
+civ_rat_spawns = 1
+
+air_traffic_flight_level = 330
+--example:  200 = 20000ft
+
+max_distance_destination = 5000
+--the max distance in KM
+--of the ai aircaft's final destination airfield, which are chosen at random
+
+civ_allow_immortal = false
+civ_allow_invisible = true
+
+-- enable or disable ai aircraft types that will spawn in (use true or false)
+-- civilian
+enable_yak = false
+enable_c130 = true
+enable_c5 = true
+enable_an26 = true
+enable_a320 = true
+enable_a380 = true
+enable_b737 = true
+
+airbase_1 = "Sochi-Adler"
+airbase_2 = "Tbilisi-Lochini"
+airbase_3 = "Mineralnye Vody"
+airbase_4 = "Sukhumi-Babushara"
+
+inactive_timer = 300
+
+if random_civ_air_traffic == true then
+
+  if enable_yak == true then
+    local yak=RAT:New("RAT_YAK")
+    yak:SetDeparture({airbase_1, airbase_2, airbase_3, airbase_4})
+    --yak:SetMaxDistance(max_distance_destination/10)
+    yak:SetDestination({airbase_1, airbase_2, airbase_3, airbase_4})
+    yak:TimeDestroyInactive(inactive_timer)
+    yak:ATC_Messages(false)
+    yak:SetFLcruise(air_traffic_flight_level)
+    if civ_allow_immortal == true then
+      yak:Immortal()
+    end
+    if civ_allow_invisible == true then
+      yak:Invisible()
+    end
+    yak:Spawn(civ_rat_spawns)
+  end
+
+  if enable_c130 == true then
+    local c130=RAT:New("RAT_C130")
+    c130:SetDeparture({airbase_1, airbase_2, airbase_3, airbase_4})
+    --c130:SetMaxDistance(max_distance_destination/5)
+    c130:SetDestination({airbase_1, airbase_2, airbase_3, airbase_4})
+    c130:TimeDestroyInactive(inactive_timer)
+    c130:ATC_Messages(false)
+    c130:SetFLcruise(air_traffic_flight_level)
+    if civ_allow_immortal == true then
+      c130:Immortal()
+    end
+    if civ_allow_invisible == true then
+      c130:Invisible()
+    end
+    c130:Spawn(civ_rat_spawns)
+  end
+
+  if enable_c5 == true then
+    local c5 =RAT:New("RAT_C5")
+    c5:SetDeparture({ airbase_1, airbase_2, airbase_3, airbase_4})
+    --c5:SetMaxDistance(max_distance_destination)
+    c5:SetDestination({airbase_1, airbase_2, airbase_3, airbase_4})
+    c5:TimeDestroyInactive(inactive_timer)
+    c5:ATC_Messages(false)
+    c5:SetFLcruise(air_traffic_flight_level)
+    if civ_allow_immortal == true then
+      c5:Immortal()
+    end
+    if civ_allow_invisible == true then
+      c5:Invisible()
+    end
+    c5:Spawn(civ_rat_spawns)
+  end
+
+  if enable_an26 == true then
+    local an26=RAT:New("RAT_AN26")
+    an26:SetDeparture({airbase_1, airbase_2, airbase_3, airbase_4})
+    --an26:SetMaxDistance(max_distance_destination/5)
+    an26:SetDestination({airbase_1, airbase_2, airbase_3, airbase_4})
+    an26:TimeDestroyInactive(inactive_timer)
+    an26:ATC_Messages(false)
+    an26:SetFLcruise(air_traffic_flight_level)
+    if civ_allow_immortal == true then
+      an26:Immortal()
+    end
+    if civ_allow_invisible == true then
+      an26:Invisible()
+    end
+    an26:Spawn(civ_rat_spawns)
+  end
+
+  if enable_a320 == true then
+    local a320 =RAT:New("RAT_A320")
+    a320:SetDeparture({ airbase_1, airbase_2, airbase_3, airbase_4})
+    --a320:SetMaxDistance(max_distance_destination)
+    a320:SetDestination({airbase_1, airbase_2, airbase_3, airbase_4})
+    a320:TimeDestroyInactive(inactive_timer)
+    a320:ATC_Messages(false)
+    a320:SetFLcruise(air_traffic_flight_level)
+    if civ_allow_immortal == true then
+      a320:Immortal()
+    end
+    if civ_allow_invisible == true then
+      a320:Invisible()
+    end
+    a320:Spawn(civ_rat_spawns)
+  end
+
+  if enable_a380 == true then
+    local a380 =RAT:New("RAT_A380")
+    a380:SetDeparture({ airbase_1, airbase_2, airbase_3, airbase_4})
+    --a380:SetMaxDistance(max_distance_destination)
+    a380:SetDestination({airbase_1, airbase_2, airbase_3, airbase_4})
+    a380:TimeDestroyInactive(inactive_timer)
+    a380:ATC_Messages(false)
+    a380:SetFLcruise(air_traffic_flight_level)
+    if civ_allow_immortal == true then
+      a380:Immortal()
+    end
+    if civ_allow_invisible == true then
+      a380:Invisible()
+    end
+    a380:Spawn(civ_rat_spawns)
+  end
+
+  if enable_b737 == true then
+    local b737 =RAT:New("RAT_B737")
+    b737:SetDeparture({ airbase_1, airbase_2, airbase_3, airbase_4})
+    --b737:SetMaxDistance(max_distance_destination)
+    b737:SetDestination({airbase_1, airbase_2, airbase_3, airbase_4})
+    b737:TimeDestroyInactive(inactive_timer)
+    b737:ATC_Messages(false)
+    b737:SetFLcruise(air_traffic_flight_level)
+    if civ_allow_immortal == true then
+      b737:Immortal()
+    end
+    if civ_allow_invisible == true then
+      b737:Invisible()
+    end
+    b737:Spawn(civ_rat_spawns)
+  end
+  timer.scheduleFunction(function()
+    trigger.action.outText("Random Civilian Air Traffic is ENABLED...", 10)
+  end, nil, timer.getTime() + 8  )
+
+
+end
+
+if random_civ_air_traffic == false then
+  timer.scheduleFunction(function()
+    trigger.action.outText("Random Civilian Air Traffic is DISABLED...", 10)
+  end, nil, timer.getTime() + 8  )
+
+end
+
+
+env.info('RAT Script complete')
