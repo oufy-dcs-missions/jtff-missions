72da05407bfac205ea8350c50c06b65e3bb6d0ff
Sebastien LONGO <sebastien.longo@oufy.net>
2021-11-13 11:46:41 +0100

    move jtff-template to a separated repo

--- template/settings/persiangulf/fox-zone-persian.lua
+++
@@ -1,9 +0,0 @@
-FoxRangesConfig= {
-    {
-        name = 'East',
-        enable = false,
-        engageZoneGroupName = 'training-zone-pvp',
-        debug = false,
-        missilemessages = true
-    }
-}
